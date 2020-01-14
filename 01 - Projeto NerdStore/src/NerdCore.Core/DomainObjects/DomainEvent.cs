6065269dfebb1966c208a1dbdf07aefa1a74d1c6
Thiago Borges <thiagoborges@amhp.com.br>
2020-01-14 15:18:33 -0300

    Aula - Lançando eventos de domínio

---
+++ 01 - Projeto NerdStore/src/NerdCore.Core/DomainObjects/DomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using NerdCore.Core.Messages;
+using System;
+
+namespace NerdCore.Core.DomainObjects
+{
+    public class DomainEvent : Event
+    {
+        public DomainEvent(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+        }
+    }
+}
--- 01 - Projeto NerdStore/src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ 01 - Projeto NerdStore/src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +10,17 @@ namespace NerdStore.Catalogo.Domain
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IMediatrHandler _bus;
 
-        public EstoqueService(IProdutoRepository produtoRepository)
+        public EstoqueService(IProdutoRepository produtoRepository, IMediatrHandler bus)
         {
             _produtoRepository = produtoRepository;
+            _bus = bus;
         }
 
         public void Dispose()
         {
-            _produtoRepository?.Dispose();
+            _produtoRepository?.Dispose();            
         }
 
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
@@ -28,6 +32,13 @@ namespace NerdStore.Catalogo.Domain
             if (!produto.PossuiEstoque(quantidade)) return false;
 
             produto.DebitarEstoque(quantidade);
+
+            //TODO: Parametrizar a quantidade de estoque baixo
+            if(produto.QuantidadeEstoque < 10)
+            {
+                //Avisar, mandar e-mail, abrir chamado, ralizar compra
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+            }
 
             return await _produtoRepository.UnitOfWork.Commit();
         }        
