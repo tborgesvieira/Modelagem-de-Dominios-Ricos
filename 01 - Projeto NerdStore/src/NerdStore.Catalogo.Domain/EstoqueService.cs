84b5e0d85a977845685790c2cce7a999f34ae7ec
Thiago Borges <thiagoborges@amhp.com.br>
2020-01-14 13:40:00 -0300

    Aula - Utilizando serviços de domínio

---
+++ 01 - Projeto NerdStore/src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class EstoqueService : IEstoqueService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public EstoqueService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public void Dispose()
+        {
+            _produtoRepository?.Dispose();
+        }
+
+        public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto is null) return false;
+
+            if (!produto.PossuiEstoque(quantidade)) return false;
+
+            produto.DebitarEstoque(quantidade);
+
+            return await _produtoRepository.UnitOfWork.Commit();
+        }        
+
+        public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto is null) return false;
+
+            if (!produto.PossuiEstoque(quantidade)) return false;
+
+            produto.ReporEstoque(quantidade);
+
+            return await _produtoRepository.UnitOfWork.Commit();
+        }
+    }
+}
