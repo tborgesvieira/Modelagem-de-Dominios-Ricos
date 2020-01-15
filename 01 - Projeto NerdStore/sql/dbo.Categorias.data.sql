c53aa0d739de80f5d606b4177ecd4c076b918631
Thiago Borges <thiagoborges@amhp.com.br>
2020-01-15 08:09:06 -0300

    Aula - Desenvolvendo a apresentação - Validando o funcionamento

---
+++ 01 - Projeto NerdStore/sql/dbo.Categorias.data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Categorias] ([Id], [Nome], [Codigo]) VALUES (N'af44f24b-2a5f-4c86-b77e-28f9730307ef', N'Adesivos', 102)
+INSERT INTO [dbo].[Categorias] ([Id], [Nome], [Codigo]) VALUES (N'ca6b0fe4-73ca-480f-8386-44dec487a2b0', N'Camisas', 100)
+INSERT INTO [dbo].[Categorias] ([Id], [Nome], [Codigo]) VALUES (N'458c8428-08c9-4050-8f2c-f3ea2adebe6d', N'Canecas', 101)
